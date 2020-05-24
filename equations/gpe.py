4719ac5c7cbdf995057061edb8d4de337a08b0a3
Bob Greenshields <bob@thegreenshields.com>
2020-05-24 16:55:12 +0100

    Initial commit

---
+++ equations/gpe.py
@@ -0,0 +1,22 @@
+def hello():
+    """Prints hello"""
+    print("Hello Luca")
+
+class GPE:
+    def description(self):
+        """returns a description of the equation"""
+        return "Gravitational potential energy"
+
+    def call(self):
+        """asks for variables and calculates value"""
+        h = input("Enter height: ")
+        print(h)
+        
+
+
+
+if __name__ == "__main__":
+    hello()
+    gpe = GPE()
+    print(gpe.description())
+    gpe.call()
